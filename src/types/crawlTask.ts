e3eb5ab6f06a6dcf7c209c039f98c3a0f4e749f0
Daniel <danieldimitrov2304@gmail.com>
2024-11-23 10:35:49 +0200

    Clear workspace

--- src/types/crawlTask.ts
+++
@@ -1,9 +0,0 @@
-export default interface CrawlTask {
-    id: number;
-    site_id: number;
-    status: 'pending' | 'in-progress' | 'cancelled' | 'completed' | 'error';
-    created_at: Date;
-    completed_at?: Date;
-    data?: Record<string, unknown>;
-    error?: string;
-}
--- src/workers/baseWorker.ts
+++ src/workers/baseWorker.ts
@@ -7,77 +7,16 @@
  */
 
 // Import dependencies
-import { parentPort, workerData } from 'worker_threads';
-import WorkerData from '../types/workerData';
-import WorkerMessage from '../types/workerMessage';
-import logger from '../utils/logger';
 
 // BaseWorker class
 export default class BaseWorker {
 
-    // Properties
-    public context: WorkerData;
-
     // Constructor
     constructor() {
-
-        // No parent port
-        if (!parentPort)
-            throw new Error('Not running in a worker thread');
-
-        // Initialize the context
-        this.context = workerData;
-
-        // Automatically listen for incoming messages
-        parentPort.on('message', async (message: { call: string }) => {
-
-            // Extract the call from the message
-            const { call } = message;
-
-            // Prepare for errors
-            try {
-
-                // Dynamically invoke the method
-                await (this as any)[call]();
-
-            } 
-
-            // Catch errors
-            catch (error) {
-
-                // Log the error
-                logger.error(`Error in worker call '${call}':`, error);
-
-                // Build an error message
-                const errorMessage: WorkerMessage = {
-                    status: 'error',
-                    error: (error as Error).message,
-                };
-
-                // Publish the error message
-                await this.publishMessage(errorMessage);
-            }
-        });
+       
+        throw new Error('Method not implemented.');
     }
 
-    // Publish a message to the parent thread
-    public async publishMessage(message: WorkerMessage) {
-
-        // Prepare for errors
-        try {
-
-            // Send the message to the parent thread
-            parentPort?.postMessage(message);
-
-        } 
-        
-        // Catch errors
-        catch (error) {
-
-            // Log the error
-            logger.error(error);
-        }
-    }
 
     // Abstract method for derived workers
     protected run(): Promise<void> {
