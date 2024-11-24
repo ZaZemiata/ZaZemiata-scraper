fc2e9a5a761613394ea1e9acd9c19f47ac5cd3e1
Daniel <danieldimitrov2304@gmail.com>
2024-11-24 08:08:54 +0200

    Decrement active workers twice fixed

--- src/bot.ts
+++ src/bot.ts
@@ -110,8 +110,6 @@ export const crawlPendingTasks = async (): Promise<void> => {
                 });
             }
 
-            // Decrement active worker count
-            activeWorkers--;
         });
 
         // Listen for worker exit
