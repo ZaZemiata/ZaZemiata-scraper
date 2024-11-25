4da6425cb50ba1e08957558c9cb02646c757fbf0
100QN <83006473+100QNKolev@users.noreply.github.com>
2024-11-25 14:56:41 +0200

    Created source seeder

--- src/db/prisma/seeders/sourceSeeder.ts
+++ src/db/prisma/seeders/sourceSeeder.ts
@@ -3,10 +3,39 @@ import logger from "../../../utils/logger";
 
 async function seedSources() {
 
-    // Seeder not implemented
-    throw new Error('Method not implemented.');
-}
+    // Delete all records from sources table
+    await prisma.sources.deleteMany();
 
+    await prisma.sources.createMany({
+        data: [
+            {
+                id: 100,
+                site_name: 'Ruse',
+                worker_name: 'Ruse',
+                scrape_frequency_seconds: 5,
+                active: true,
+                created_at: new Date()
+            },
+            {
+                id: 200,
+                site_name: 'Sofia',
+                worker_name: 'Sofia',
+                scrape_frequency_seconds: 5,
+                active: true,
+                created_at: new Date()
+            },
+            {
+                id: 300,
+                site_name: 'Plovdiv',
+                worker_name: 'Plovdiv',
+                scrape_frequency_seconds: 5,
+                active: true,
+                created_at: new Date()
+            }
+        ]
+    });
+}
+    
 // Seed sources
 seedSources().catch((error) => {
 
