2e248ac399d66f62e2fc84e9309287f2b96b73e6
100QN <83006473+100QNKolev@users.noreply.github.com>
2024-11-25 15:34:37 +0200

    Created file with constants for source ids

---
+++ src/db/prisma/constants.ts
@@ -0,0 +1,6 @@
+// Make sure to check if these values are the same as the source Ids in the database
+export const SOURCE_IDS = {
+    RUSE: BigInt(100),
+    SOFIA: BigInt(200),
+    PLOVDIV: BigInt(300),
+  };
